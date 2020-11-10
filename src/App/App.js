f4782a7779c677ee703e56351030cf6023fbe2e2
Jonathon Joyner <jonathonandon@gmail.com>
2020-11-09 20:06:02 -0600

    added changes to setup auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import firebaseApp from '../Helpers/Data/connection';
 import './App.scss';
+import Auth from '../Components/Auth';
+import MyNavbar from '../Components/MyNavbar';
+import BoardContainer from '../Components/BoardContainer';
 
+firebaseApp();
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: true });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      let component = '';
+      if (authed) {
+        component = <BoardContainer />;
+      } else {
+        component = <Auth />;
+      }
+      return component;
+    };
+
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <MyNavbar authed={authed} />
+        {loadComponent()}
       </div>
     );
   }
